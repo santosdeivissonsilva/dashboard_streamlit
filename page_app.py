85e0f3823d81ae417607f221971fc459f2d2a4d3
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2023-06-17 19:39:07 -0300

    add selectbox and cahce

--- page_app.py
+++ page_app.py
@@ -7,7 +7,18 @@ with st.container():
     st.title('Dashboard de Contratos')
     st.write('Informações sobre os contratos fechados ao longo de Maio/2023')
 
+
+@st.cache_data
+def carregar_dados():  # Função utilizada para carregar dados
+    tabela = pd.read_csv('resultados.csv')
+    return tabela
+
+
 with st.container():
     st.write('---')
-    dados = pd.read_csv("resultados.csv")
+    quant_dias = st.selectbox('Selecione um período', [
+                              "7D", "15D", "30D"])
+    num_dias = int(quant_dias.replace("D", ""))
+    dados = carregar_dados()
+    dados = dados[-num_dias:]
     st.area_chart(dados, x="Data", y="Contratos")
